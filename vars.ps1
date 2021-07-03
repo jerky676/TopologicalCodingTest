a4b3acb9811059d1b844a391223e73d0a1518ecc
Justin Luther <8236331+jerky676@users.noreply.github.com>
2021-07-03 14:59:05 -0500

    wip

--- vars.ps1
+++ vars.ps1
@@ -1,8 +1,56 @@
-$testnonempty="$PSScriptRoot/NonEmptyDataSet";
-$testcircular="$PSScriptRoot/CircularDataSet";
-$testworking1="$PSScriptRoot/WorkingDataSet1";
-$testworking2="$PSScriptRoot/WorkingDataSet2";
+. ("$PSScriptRoot/TestSetup.ps1")
 
-$testdirectories=@($testnonempty,$testcircular,$testworking1,$testworking2);
+$testnonempty=New-Object TestSetup;
+$testnonempty.dirname="NonEmptyFilesDataSet";
+$testnonempty.data=@{
+    "a" = @("c","e"),
+    "b" = @("e", "c"),
+    "c" = @("d","e"),
+    "d" = @("c","e"),
+    "e" = @("c",)
+};
+
+
+$testcircular=New-Object TestSetup;
+$testcircular.dirname="$PSScriptRoot/CircularDataSet";
+$testcircular.data=@{
+    "a" = @("c","e"),
+    "b" = @("e", "c"),
+    "c" = @("d","e", "a"),
+    "d" = @("c","e"),
+    "e" = @()
+};
+
+$testworking1=New-Object TestSetup;
+$testworking1.dirname="$PSScriptRoot/WorkingDataSet1";
+$testworking1.data=@{
+    "a" = @("c","e"),
+    "b" = @("e", "c"),
+    "c" = @("d","e"),
+    "d" = @("c","e"),
+    "e" = @("c",)
+};
+
+$testworking2=New-Object TestSetup;
+$testworking2.dirname="$PSScriptRoot/WorkingDataSet2";
+$testworking2.data=@{
+    "a" = @("c","e"),
+    "b" = @("e", "c", "f"),
+    "c" = @("d","e"),
+    "d" = @("c","e", "f"),
+    "e" = @(),
+    "f" = @()
+};
+
+
+
+
+
+# $testnonempty="$PSScriptRoot/NonEmptyDataSet";
+# $testcircular="$PSScriptRoot/CircularDataSet";
+# $testworking1="$PSScriptRoot/WorkingDataSet1";
+# $testworking2="$PSScriptRoot/WorkingDataSet2";
+
+$testsetups=@($testnonempty,$testcircular,$testworking1,$testworking2);
 
 
