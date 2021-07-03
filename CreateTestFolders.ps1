81558d2543e4e2ade39316ac8957b39fa4d0f005
Justin Luther <8236331+jerky676@users.noreply.github.com>
2021-07-03 15:03:29 -0500

    wip

--- CreateTestFolders.ps1
+++ CreateTestFolders.ps1
@@ -1,10 +1,18 @@
 . ("$PSScriptRoot/vars.ps1")
 
 
-
-
-
-
 foreach ($testdirectory in $testdirectories) {
-    New-Item -ItemType Directory -Force -Path $testdirectory   
+    New-Item -ItemType Directory -Force -Path $testdirectory.dirname;
+    
+    foreach ($file in $testdirectory.data) {
+        $filename="$PSScriptRoot/$file.txt"
+
+        Add-Content $filename $file.values
+
+        # foreach ($lines in $file.values) {
+            
+        # }
+        
+    }
+
 }
